3538b50b8d2f16fd872a1d62892b23ef901de693
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-08-20 16:32:13 -0600

    feat: use VX-7 v4 production weapon

--- scripts/player_v3.gd
+++ scripts/player_v3.gd
@@ -1,6 +1,6 @@
 extends "res://scripts/player_v2.gd"
 
-const WEAPON_V3: Script = preload("res://scripts/weapon_v3.gd")
+const WEAPON_V4: Script = preload("res://scripts/weapon_v4.gd")
 
 
 func _build_camera_and_weapon() -> void:
@@ -13,7 +13,7 @@ func _build_camera_and_weapon() -> void:
 	add_child(camera)
 
 	weapon = Node3D.new()
-	weapon.name = "VX7_Rifle_Production"
-	weapon.set_script(WEAPON_V3)
+	weapon.name = "VX7_Rifle_Production_V4"
+	weapon.set_script(WEAPON_V4)
 	weapon.connect("ammo_changed", Callable(self, "_on_weapon_ammo_changed"))
 	camera.add_child(weapon)
